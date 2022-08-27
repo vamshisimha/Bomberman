ee04cca2b126ae0546c301a2bda17984f608c89a
LAPTOP-4UDAE21C\VAMSHI <78913213+vamshisimha@users.noreply.github.com>
2022-08-27 12:09:57 +0530

    1st push

---
+++ Assets/Scripts/DestroySelf.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+using UnityEngine;
+using System.Collections;
+
+
+public class DestroySelf : MonoBehaviour
+{
+    public float Delay = 3f;
+    
+
+    void Start ()
+    {
+        Destroy (gameObject, Delay);
+    }
+}
---
+++ Assets/Scripts/GlobalStateManager.cs
@@ -0,0 +1,46 @@
+﻿
+
+using UnityEngine;
+using System.Collections;
+
+public class GlobalStateManager : MonoBehaviour
+{
+
+    private int deadPlayers = 0;
+private int deadPlayerNumber = -1;  
+    public void PlayerDied (int playerNumber)
+    {
+deadPlayers++; // 1
+
+if (deadPlayers == 1) 
+{ // 2
+    deadPlayerNumber = playerNumber; // 3
+    Invoke("CheckPlayersDeath", .3f); // 4
+}  
+
+    }
+
+
+    void CheckPlayersDeath() 
+{
+  // 1
+  if (deadPlayers == 1) 
+  { 
+    // 2
+    if (deadPlayerNumber == 1) 
+    { 
+      Debug.Log("Player 2 is the winner!");
+    // 3
+    } 
+    else 
+    { 
+      Debug.Log("Player 1 is the winner!");
+    }
+     // 4
+  } 
+  else 
+  { 
+    Debug.Log("The game ended in a draw!");
+  }
+}  
+}
